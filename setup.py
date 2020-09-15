1d04eaa165a0a3b8739e41b9499e538c2c0a1efb
paddyjclancy <patrickclancy0605@gmail.com>
2020-09-15 17:40:32 +0100

    Merging dataframe to title / artist info

--- setup.py
+++ setup.py
@@ -56,3 +56,16 @@ for track in playlist_tracks_data['items']:
 
 features = sp.audio_features(playlist_tracks_id)
 features_df = pd.DataFrame(data=features, columns=features[0].keys())
+
+
+# Merging Dataframe with Title / Artist info
+features_df['title'] = playlist_tracks_titles
+features_df['first_artist'] = playlist_tracks_first_artists
+features_df['all_artists'] = playlist_tracks_artists
+# features_df = features_df.set_index('id')
+features_df = features_df[['id', 'title', 'first_artist', 'all_artists',
+                           'danceability', 'energy', 'key', 'loudness',
+                           'mode', 'acousticness', 'instrumentalness',
+                           'liveness', 'valence', 'tempo',
+                           'duration_ms', 'time_signature']]
+features_df.tail()
